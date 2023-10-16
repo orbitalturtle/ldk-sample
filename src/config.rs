9427f39628fe5eab131454ee9fc264263f25362a
Orbital <orbitalturtle@protonmail.com>
2023-10-16 14:10:41 -0500

    Tweak log dir name

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,4 @@
 use bitcoin::network::constants::Network;
-use chrono::Utc;
 use lightning::ln::msgs::NetAddress;
 use std::fs;
 use std::path::PathBuf;
@@ -20,16 +19,14 @@ pub struct LdkUserInfo {
 // Here we initialize three layers of directories needed for our tests. We won't persist ldk data, but we'll persist
 // the logs to help with debugging.
 pub(crate) fn setup_data_and_log_dirs(
-	ldk_dir: PathBuf, test_name: &str,
+	ldk_dir: PathBuf, _test_name: &str,
 ) -> (String, TempDir, String) {
 	let ldk_data_dir_binding =
 		Builder::new().prefix("ldk-data-dir").tempdir_in(ldk_dir.clone()).unwrap();
 	let ldk_data_dir = String::from(ldk_data_dir_binding.path().to_str().unwrap());
 
 	// Create the ldk-logs dir, which we'll persist after the tests are over for debugging.
-	let now_timestamp = Utc::now();
-	let timestamp = now_timestamp.format("%d-%m-%Y-%H%M");
-	let ldk_log_dir_binding = ldk_dir.join(format!("ldk-logs-{test_name}-{timestamp}"));
+	let ldk_log_dir_binding = ldk_dir.join(format!("ldk-logs"));
 	let ldk_log_dir = String::from(ldk_log_dir_binding.as_path().to_str().unwrap());
 
 	// Initialize the LDK data directory if necessary.
